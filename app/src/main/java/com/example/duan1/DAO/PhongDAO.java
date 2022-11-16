f0b6ca97e3875fbe2e7dbe815f90a48654c82f77
Duyetdtph20532 <117613152+Duyetdtph20532@users.noreply.github.com>
2022-11-16 07:54:15 +0700

    duyet tao

--- app/src/main/java/com/example/duan1/DAO/PhongDAO.java
+++ app/src/main/java/com/example/duan1/DAO/PhongDAO.java
@@ -1,4 +1,41 @@
 package com.example.duan1.DAO;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.duan1.Model.Phong;
+import com.example.duan1.database.DbHelper;
+
 public class PhongDAO {
+    DbHelper helper;
+
+    public PhongDAO(Context context) {
+        helper = new DbHelper(context);
+    }
+    public boolean insert(int soPhong, String moTa) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("soPhong", soPhong);
+        values.put("moTa", moTa);
+        long row = db.insert("PHONG", null, values);
+
+        return (row > 0);
+    }
+
+    public boolean update(Phong phong) {
+        //  String sql="update phong set sophong=?,mota=? where maPhong";
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("soPhong", phong.getSoPhong());
+        values.put("mota", phong.getMoTa());
+        int row = db.update("phong", values, " maPhong=?", new String[]{phong.getMaPhong() + ""});
+        return (row > 0);
+    }
+
+    public boolean delete(int maPhong) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        int row = db.delete("phong", "maPhong=?", new String[]{maPhong + ""});
+        return (row > 0);
+    }
 }
--- app/src/main/java/com/example/duan1/Model/Phong.java
+++ app/src/main/java/com/example/duan1/Model/Phong.java
@@ -1,4 +1,40 @@
 package com.example.duan1.Model;
 
 public class Phong {
+    private int maPhong;
+    private int soPhong;
+    private String moTa ;
+
+    public Phong() {
+    }
+
+    public Phong(int maPhong, int soPhong, String moTa) {
+        this.maPhong = maPhong;
+        this.soPhong = soPhong;
+        this.moTa = moTa;
+    }
+
+    public int getMaPhong() {
+        return maPhong;
+    }
+
+    public void setMaPhong(int maPhong) {
+        this.maPhong = maPhong;
+    }
+
+    public int getSoPhong() {
+        return soPhong;
+    }
+
+    public void setSoPhong(int soPhong) {
+        this.soPhong = soPhong;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
 }
